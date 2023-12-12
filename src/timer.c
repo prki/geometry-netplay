39ae998e6ef23c20eda5fb85525aacbd8d0aa13e
prki <mar.pikna@gmail.com>
2023-12-12 23:32:45 +0000

    Timer struct + FPS calc

---
+++ src/timer.c
@@ -0,0 +1,44 @@
+#include "timer.h"
+
+#include <SDL.h>
+#include <stdio.h>
+#include <string.h>
+
+void f_timer_init(F_Timer* timer) {
+  timer->current_time = SDL_GetTicks64();
+  timer->previous_time = 0;
+  timer->delta_time = timer->current_time - timer->previous_time;
+  timer->times_index = 0;
+
+  memset(timer->times, 0, sizeof(Uint64) * F_TIMER_HISTORY_SIZE);
+}
+
+// Internal/private function for working with the history time sample
+void f_timer_update_times(F_Timer* timer) {
+  if (timer->times_index >= F_TIMER_HISTORY_SIZE) {
+    timer->times_index = 0;
+  }
+
+  timer->times[timer->times_index] = timer->delta_time;
+  timer->times_index += 1;
+}
+
+void f_timer_update(F_Timer* timer) {
+  timer->previous_time = timer->current_time;
+  timer->current_time = SDL_GetTicks64();
+  timer->delta_time = timer->current_time - timer->previous_time;
+
+  f_timer_update_times(timer);
+}
+
+double f_timer_calc_fps(F_Timer* timer) {
+  Uint64 deltas = 0;
+  for (size_t i = 0; i < F_TIMER_HISTORY_SIZE; i++) {
+    deltas += timer->times[i];
+  }
+  double avg_delta = (double)deltas / (double)F_TIMER_HISTORY_SIZE;
+  printf("avg_delta: %f\n", avg_delta);
+
+  double ret = (1000.0 / avg_delta);
+  return ret;
+}
---
+++ src/timer.h
@@ -0,0 +1,19 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+#include <SDL.h>
+
+#define F_TIMER_HISTORY_SIZE 100
+
+typedef struct {
+  Uint64 current_time;   // time at initializing F_Timer or after updating
+  Uint64 previous_time;  // previous time. 0 upon initialization
+  Uint64 delta_time;     // current - previous utility
+  Uint64 times[F_TIMER_HISTORY_SIZE];  // sampling for FPS
+  size_t times_index;
+} F_Timer;
+
+void f_timer_init(F_Timer* timer);
+void f_timer_update(F_Timer* timer);
+double f_timer_calc_fps(F_Timer* timer);
+
+#endif  // _TIMER_H_
